aa4ac965b79e79332563d6f5fc31ac5110c56e4f
mmtakeuchi <mikemtaki@gmail.com>
2021-05-17 13:50:21 -0700

    create method to get random food from data

--- what_to_eat/src/App.js
+++ what_to_eat/src/App.js
@@ -1,9 +1,14 @@
 import "./App.css";
+import { Food, Prices } from "./api/data";
 
 function App() {
+  let index = Math.floor(Math.random() * Object.keys(Food).length);
   return (
     <div className="App">
       <h1>What To Eat</h1>
+      <div>{Object.keys(Food)[index]}:</div>
+      <div>{Object.values(Food)[index]}</div>
+      <div>{Prices[0]}</div>
     </div>
   );
 }

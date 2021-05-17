f030edd9d4e9532021f44d81d920a975a010938e
mmtakeuchi <mikemtaki@gmail.com>
2021-05-17 14:15:37 -0700

    iconvert css to scss files

--- what_to_eat/src/App.js
+++ what_to_eat/src/App.js
@@ -1,14 +1,16 @@
-import "./App.css";
+import "./App.scss";
 import { Food, Prices } from "./api/data";
 
 function App() {
-  let index = Math.floor(Math.random() * Object.keys(Food).length);
+  let foodIndex = Math.floor(Math.random() * Object.keys(Food).length);
+  let randomPrice = Prices[Math.floor(Math.random() * Prices.length)];
+
   return (
     <div className="App">
       <h1>What To Eat</h1>
-      <div>{Object.keys(Food)[index]}:</div>
-      <div>{Object.values(Food)[index]}</div>
-      <div>{Prices[0]}</div>
+      <div className="food">{Object.keys(Food)[foodIndex]}:</div>
+      <div>{Object.values(Food)[foodIndex]}</div>
+      <div>{randomPrice}</div>
     </div>
   );
 }
